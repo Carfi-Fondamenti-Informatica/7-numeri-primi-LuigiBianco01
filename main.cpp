691aaa64cf8301fc2db9ed40b15a0db0eaabb2ec
LuigiBianco01 <92793421+LuigiBianco01@users.noreply.github.com>
2021-12-01 23:51:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+int main() {
+    char carattereinserito;
+    char caratterestampa;
+
+    cin >> carattereinserito;
+
+    if (funzione(carattereinserito)==true){
+        if (carattereinserito>=65&&carattereinserito<=90) {
+            caratterestampa = carattereinserito + 32;
+        } else {
+            caratterestampa = carattereinserito - 32;
+        }
+        cout << caratterestampa;
+    } else {
+        cout << "errore";
+    }
+    return 0;
+
 }
