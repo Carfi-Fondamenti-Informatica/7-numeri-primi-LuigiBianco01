185ba3b8c6e29af35b13ff614a099cc14eea3599
LuigiBianco01 <92793421+LuigiBianco01@users.noreply.github.com>
2021-12-01 23:52:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
 
+bool funzione(char carattereinserito){
+    char caratterestampa;
+    bool risultato;
+    if (carattereinserito>=65&&carattereinserito<=90 or carattereinserito>=97&&carattereinserito<=122){
+        risultato=true;
+    } else {
+        risultato=false;
+    }
+    return risultato;
+}
